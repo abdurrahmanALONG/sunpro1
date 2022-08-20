df6121a614c0ff5b559b9d70e23d2d7b1b3687f1
abdurrahmanALONG <abdullahhanxin@gmail.com>
2022-08-20 18:09:12 +0800

    bsaic instlation and routing done

---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is for About</h1>
+        </div>
+    );
+};
+
+export default About;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Banner from './Banner'
+import Course from './Course';
+import Footer from './Footer';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home page</h1>
+            <div className="gap-y-5">
+                <Banner></Banner>
+            </div>
+            <div className="gap-y-5">
+                <Course></Course>
+            </div>
+            <div>
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
